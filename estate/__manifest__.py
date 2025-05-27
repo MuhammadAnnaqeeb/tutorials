2f4e662ad8a22e075503d4c9a0faa7956c2f5037
M.elnakeeb <melnakeeb@teec-f6-11.asuh.local>
2025-05-27 11:57:08 +0300

    Fix Cyclic dependency that does not allow admin to edit his user details because he is not member of Borkerage/Agent nor Brokerage/Manager. Solution was to imply tht every system admin is Brokerage manager

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -20,8 +20,8 @@
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
         'views/res_users_views.xml',
-        'security/ir.model.access.csv',
         'security/security.xml',
+        'security/ir.model.access.csv',
 
     ],
     'demo': [
